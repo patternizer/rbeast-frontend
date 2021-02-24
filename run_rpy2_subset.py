c46568d89c83516d6ec42ce421dd004ceafeddae
Michael Taylor <patternizer@gmail.com>
2021-02-24 16:38:19 +0000

    Set path root to checkouts

--- run_rpy2_subset.py
+++ run_rpy2_subset.py
@@ -30,34 +30,35 @@ import subprocess
 def make_shell_command(filename,directory,stationcode):
          
     currentdir = os.getcwd()
-    outdir = '{0}/{1:06d}'.\
+    outdir = '{0}/{1}'.\
         format(currentdir,stationcode)
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     os.chdir(outdir)
     try:
-        os.symlink('/users/mtaylor/checkouts/rbeast-frontend/rbeast-frontend-runnable-rpy2-subset.py','rbeast-frontend-runnable-rpy2-subset.py')
+        os.symlink('~/checkouts/rbeast-frontend/rbeast-frontend-runnable-rpy2-subset.py','rbeast-frontend-runnable-rpy2-subset.py')
+        os.symlink('~/checkouts/rbeast-frontend/Iceland21.postmerge','Iceland21.postmerge')
+        os.symlink('~/checkouts/rbeast-frontend/normals5.GloSAT.prelim03_FRYuse_ocPLAUS1_iqr3.600reg0.3_19411990_MIN15_OCany_19611990_MIN15_PERDEC00_NManySDreq.txt','normals5.GloSAT.prelim03_FRYuse_ocPLAUS1_iqr3.600reg0.3_19411990_MIN15_OCany_19611990_MIN15_PERDEC00_NManySDreq.txt')
     except:
         pass
 
     # job submission script file 
-    job_file = 'run.{0:06d}.sh'.format(stationcode)
+    job_file = 'run.{0}.sh'.format(stationcode)
     with open(job_file,'w') as fp:
-        job_id = 'SBATCH --job-name=mataylor.{0:06d}'.format(stationcode)
-        job_str = 'python rbeast-frontend-runnable-rpy2-subset.py {0} {1:06d}\n'.format(filename,stationcode)
-        fp.write('#!/bin/bash')
-        fp.write('SBATCH --partition=short-serial')
+        job_id = 'SBATCH --job-name=mataylor.{0}\n'.format(stationcode)
+        job_str = 'python rbeast-frontend-runnable-rpy2-subset.py {0} {1}\n'.format(filename,stationcode)
+        fp.write('#!/bin/bash\n')
+        fp.write('SBATCH --partition=short-serial\n')
         fp.write(job_id)
-        fp.write('SBATCH -o %j.out')
-        fp.write('SBATCH -e %j.err') 
-        fp.write('SBATCH --time=30:00')
-        fp.write('conda activate')
-        fp.write('conda activate renv')
+        fp.write('SBATCH -o %j.out\n')
+        fp.write('SBATCH -e %j.err\n') 
+        fp.write('SBATCH --time=30:00\n')
+        fp.write('conda activate\n')
+        fp.write('conda activate renv\n')
         fp.write(job_str)    
 
     os.chmod(job_file,stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
-    job_name='./'+job_file
-    job = ['sbatch',job_name]
+    job = ['sbatch',job_file]
     subprocess.call(job)
     os.chdir(currentdir)                    
 
@@ -68,7 +69,7 @@ def run_all_stations(filename):
     #-----------------------------------
     stationcode = '040300'
 
-    directory = '/users/mtaylor/checkouts/rbeast-frontend/{0:06d}'.format(stationcode)
+    directory = '~/checkouts/rbeast-frontend/{0}'.format(stationcode)
     make_shell_command(filename,directory,stationcode)       
  
 if __name__ == "__main__":
